c14d574fc5ebb62ae3664cfedf560a066d503c04
Oscar Gomez <oscar@Oscars-iMac.local>
2014-09-08 09:48:54 -0700

    Commented out code to add dummy data on startup

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
 Meteor.startup(function() {
  
-  if(CalEvents.find().fetch().length === 0) {
-
-    var startDateTime = new Date();
-    var endDateTime = moment(new Date(startDateTime)).add(1, 'h').toISOString();
-    endDateTime = new Date(endDateTime);
-
+    /***** Uncomment block below to populate calendar with dummy data on startup ******/
+    /*
+    if(CalEvents.find().fetch().length === 0) {
     
-    console.log('Creating dummy requests: ');
- 
-    var events = [];
-
-    for(var i = 0; i < 1000; i++) {    
-        CalEvents.insert({title: 'New Event' + i, start: startDateTime, end: endDateTime });
+        var startDateTime = new Date();
+        var endDateTime = moment(new Date(startDateTime)).add(1, 'h').toISOString();
+        endDateTime = new Date(endDateTime);
         
-        startDateTime = endDateTime;
+        console.log('Creating dummy requests: ');
         
-        if(i % 5 == 0) {
-            startDateTime = moment(new Date(startDateTime)).add(1, 'd').hour(0).toISOString();
-            startDateTime = new Date(startDateTime);
+        var events = [];
         
-            endDateTime = moment(new Date(startDateTime)).add(1, 'h').toISOString();
-            endDateTime = new Date(endDateTime);
-        } else {
-            endDateTime = moment(endDateTime).add(1, 'h').toISOString();    
-            endDateTime = new Date(endDateTime);
+        for(var i = 0; i < 1000; i++) {    
+            CalEvents.insert({title: 'New Event' + i, start: startDateTime, end: endDateTime });
+            
+            startDateTime = endDateTime;
+            
+            if(i % 5 == 0) {
+                startDateTime = moment(new Date(startDateTime)).add(1, 'd').hour(0).toISOString();
+                startDateTime = new Date(startDateTime);
+            
+                endDateTime = moment(new Date(startDateTime)).add(1, 'h').toISOString();
+                endDateTime = new Date(endDateTime);
+            } else {
+                endDateTime = moment(endDateTime).add(1, 'h').toISOString();    
+                endDateTime = new Date(endDateTime);
+            }
         }
+    
     }
-
-  } 
+    */ 
 
 }); 
